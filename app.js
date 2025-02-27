b216acb2721da02550baffe3e3d99e393519e734
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-02-27 15:02:03 -0800

    fixed admin

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
+
+
 const pool = mariadb.createPool({
    host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -37,7 +40,7 @@ app.get('/', (req, res) => {
 
 app.get('/admin', async (req, res) => {
     const conn = await connect();
-    const orders = await conn.query('SELECT * FROM orders');
+    const orders = await conn.query('SELECT * FROM post');
     res.render('admin', { orders });
 });
 
