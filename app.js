23a3f280207cc964ea23b400293ce13c136dc0e6
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-02-27 14:17:03 -0800

    updated app

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 import express from 'express';
+import mariadb from 'mariadb';
+import dotenv from 'dotenv';
 
 const app = express();
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
+
+//Define function to connect to the DB
+async function connect() {
+    try {
+        const conn = await pool.getConnection();
+        console.log('Connected to the database!');
+        return conn;
+    } catch (err) {
+        console.log(`Error connecting to the database ${err}`);
+    }
+}
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -8,14 +29,16 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-const PORT = 3000;
+const PORT = process.env.APP_PORT || 3000;
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.get('/admin', async (req, res) => {
-    res.render('admin');
+    const conn = await connect();
+    const orders = await conn.query('SELECT * FROM orders');
+    res.render('admin', { orders });
 });
 
 
